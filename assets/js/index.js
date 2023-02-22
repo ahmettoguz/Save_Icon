b3ead1a543601a46f86d5573fa5d9b929340471e
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-02-23 00:29:39 +0300

    geliştiriliyor.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,22 @@
-$(function () {
-  let code = findUniCode("fa fa-car");
-  setTimeout(() => {
-    drawCanvas(code);
-  }, 3000);
-});
+let src = null;
+let color = null;
+let size = null;
 
-// this function find and returning unicode of the i
 function findUniCode(name) {
   let testI = document.createElement("i");
 
   testI.className = name;
   document.body.appendChild(testI);
 
-  let char = window
-    .getComputedStyle(testI, ":before")
-    .content.replace(/'|"/g, "");
-
+  let char = window.getComputedStyle(testI, ":before").content;
   testI.remove();
-  return "\\u" + char.charCodeAt(0).toString(16);
+
+  console.log("\\u" + char.charCodeAt(1).toString(16));
+
+  return "\\u" + char.charCodeAt(1).toString(16);
 }
 
 function drawCanvas(code) {
-//   code = "\uF064";
   let canvas = document.getElementById("canvas");
   // canvas.width = 100;
   // canvas.height = 100;
@@ -34,4 +29,26 @@ function drawCanvas(code) {
 
   // ctx.fillStyle = "white";
   ctx.fillText(code, 20, 75);
+}
+
+function changeSource() {
+  src = $("#main > div.sourceContainer > input").val();
+  console.log("source girildi:", src);
+}
+
+function changeColor() {
+  color = $("#main > div.colorContainer > input[type=color]").val();
+  console.log("color girildi:", color);
+}
+
+function changeSize() {
+  size = $("#main > div.sizeContainer > input[type=range]").val();
+  console.log("size girildi", size);
+
+  //   update output
+  $("#main > div.sizeContainer > span.result").html(`${size}px`);
+}
+
+function downloadClick() {
+  console.log("download başladı");
 }
